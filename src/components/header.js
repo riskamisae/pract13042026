2ee5d6d6be7733b5b2409cc7880c297af2e4c846
riskamisae <arinagorskova698@gmail.com>
2026-04-08 23:44:32 +0300

    styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,59 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import "./Header.css"; // <-- импорт стилей
 
 function Header({ user, setUser }) {
   const location = useLocation();
   const navigate = useNavigate();
 
   const isActive = (path) =>
-    location.pathname === path ? { fontWeight: "bold", color: "blue" } : {};
+    location.pathname === path ? { fontWeight: "700", backgroundColor: "#84A59D", color: "#F7EDE2" } : {};
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-    setUser(null);        
-    navigate("/");        
+    setUser(null);
+    navigate("/");
   };
 
   return (
-    <header style={{ display: "flex", gap: "20px", padding: "10px 20px", borderBottom: "1px solid #ccc", marginBottom: "20px" }}>
-      <Link to="/" style={isActive("/")}>Главная</Link>
-      <Link to="/cart" style={isActive("/cart")}>Корзина</Link>
-
-      {user ? (
-        <>
-          <Link to="/orders" style={isActive("/orders")}>{user.username}</Link>
-          <button
-            onClick={handleLogout}
-            style={{ marginLeft: "10px", background: "transparent", border: "none", color: "red", cursor: "pointer" }}
-          >
-            Выйти
-          </button>
-        </>
-      ) : (
-        <Link to="/auth" style={isActive("/auth")}>Вход / Регистрация</Link>
-      )}
+    <header className="header">
+      <div class="content-wrapper header-content">
+        <Link to="/" class="logo">
+          BOUQU
+        </Link>
+        <div class="head-side">
+          <div className="head-link">
+            <Link to="/" style={isActive("/")}>
+              Главная
+            </Link>
+          </div>
+          <div className="head-link">
+            <Link to="/cart" style={isActive("/cart")}>
+              Корзина
+            </Link>
+          </div>
+        </div>
+        <div class="head-side">
+          {user ? (
+            <>
+              <div class="head-link">
+                <Link to="/orders" style={isActive("/orders")}>
+                  {user.username}
+                </Link>
+              </div>
+              <button className="out-button" onClick={handleLogout}>
+                Выйти
+              </button>
+            </>
+          ) : (
+            <div className="head-link">
+              <Link to="/auth" style={isActive("/auth")}>
+                Вход / Регистрация
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </header>
   );
 }
